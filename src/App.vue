8842c4c504f020ef150b2cd32a57203c798d8fe3
Mikolaj Klarzuk <mikizuk@gmail.com>
2025-01-30 16:44:16 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    HelloWorld msg="You did it!"
+  </header>
+
+  <main>
+    TheWelcome
+  </main>
+</template>
+
+<style scoped>
+
+@media (min-width: 1024px) {
+
+}
+</style>
