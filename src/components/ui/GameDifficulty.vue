300a2fa6ad3f133c2bc4ec00aca6dee56f0b1488
Mikolaj Klarzuk <mikizuk@gmail.com>
2025-02-03 08:56:35 +0100

    Created classic parent-child component communication with ts

--- src/components/ui/GameDifficulty.vue
+++ src/components/ui/GameDifficulty.vue
@@ -1,52 +1,28 @@
 <script setup lang="ts">
 import { useSudokuStore, type Difficulty } from '@/stores/sudoku'
 import RetroButton from '@/components/slots/RetroButton.vue'
+import RetroSelect from '@/components/ui/RetroSelect.vue'
 
 const store = useSudokuStore()
 
-const onDifficultyChange = (event: Event) => {
-  const newDifficulty = (event.target as HTMLSelectElement).value as Difficulty
-
-  if (newDifficulty) {
-    store.setDifficulty(newDifficulty)
-  }
+const onDifficultyChange = (value: Difficulty): void => {
+  store.setDifficulty(value)
 }
 const onStartGame = () => {
-  // store.hideIntro()
   store.startGame()
 }
 </script>
 
 <template>
-  <label v-if="!store.isGameInprogress" for="difficulty">Set difficulty:</label>
-  <select aria-label="Difficulty:" class="select" id="difficulty" v-model="store.difficulty" @change="onDifficultyChange($event)">
-    <option v-for="difficulty in store.difficulties" :key="difficulty" :value="difficulty">
-      {{ difficulty.charAt(0).toUpperCase() + difficulty.slice(1) }}
-    </option>
-  </select>
+  <RetroSelect
+    label="Difficulty"
+    :selected="store.difficulty"
+    :options="store.difficulties"
+    @change="onDifficultyChange"
+    :hideLabel="!store.isIntroShown"
+  />
 
   <span v-if="!store.isGameInprogress" for="difficulty">And start the game!</span>
   <RetroButton v-if="!store.isGameInprogress" @click="onStartGame">Start</RetroButton>
   <RetroButton v-else @click="store.hideIntro()">Reset game</RetroButton>
 </template>
-
-<style scoped>
-.select,
-.select > option {
-  background-color: var(--soft-gray);
-  border: 2px solid var(--charcoal-gray);
-  padding: 5px 10px;
-  margin: 5px;
-  border-radius: 3px;
-  font-family: 'Press Start 2P', serif;
-  font-weight: 200;
-  font-style: normal;
-  cursor: pointer;
-  font-size: 0.7rem;
-  width: fit-content;
-
-  option {
-    background-color: var(--sky-blue);
-  }
-}
-</style>
---
+++ src/components/ui/RetroSelect.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts" generic="T">
+defineProps<{
+  label: string
+  selected: T
+  options: T[]
+  hideLabel: boolean
+}>()
+const emit = defineEmits<{
+  (e: 'change', value: T): void
+}>()
+const onSelect = (event: Event) => {
+  const newOption = (event.target as HTMLSelectElement).value as T
+  emit('change', newOption)
+}
+</script>
+
+<template>
+  <label v-if="!hideLabel" :for="`select-${label}`">Set difficulty:</label>
+  <select :aria-label="`${label}:`" class="retro-select" :id="`select-${label}`" :value="selected" @change="onSelect">
+    <option class="retro-select__option" v-for="option in options" :key="String(option)" :value="option">
+      {{ String(option).charAt(0).toUpperCase() + String(option).slice(1) }}
+    </option>
+  </select>
+</template>
+
+<style lang="scss" scoped>
+.retro-select {
+  background-color: var(--soft-gray);
+  border: 2px solid var(--charcoal-gray);
+  padding: 5px 10px;
+  margin: 5px;
+  border-radius: 3px;
+  font-family: 'Press Start 2P', serif;
+  font-weight: 200;
+  font-style: normal;
+  cursor: pointer;
+  font-size: 0.7rem;
+  width: fit-content;
+
+  .retro-select__option {
+    background-color: var(--sky-blue);
+  }
+}
+</style>
